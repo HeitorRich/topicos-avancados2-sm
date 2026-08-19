0c2973e9957ebebd5b8d9d10383c82f95f852671
HEITOR RICHARD DE OLIVEIRA LAGO <heitor.lago@cs.cruzeirodosul.edu.br>
2026-08-19 20:51:13 -0300

    novas validações

--- src/validator.ts
+++ src/validator.ts
@@ -3,6 +3,14 @@ function validarChamado(titulo: string): boolean {
         throw new Error("O título do chamado não pode ser vazio.");
     }
 
+    if (titulo.length < 5) {
+        throw new Error("O título não pode ter menos que 5 caracters");
+    }
+
+    if (titulo.length > 100) {
+        throw new Error("O título não pode ter mais que 100 caracters");
+    }
+
     return true;
 }
 
